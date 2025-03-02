01d6133e7b3e909be4cca8dd5de363d6edbfc618
Speedy <Speedimus@users.noreply.github.com>
2025-03-02 16:03:38 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from asyncio import run, gather
+from io import BytesIO
+from json import dumps
+
+from ijson import items, kvitems
+from requests import get
+
+stream_file: BytesIO = BytesIO()
+
+
+async def stream_data():
+    with get(
+            url='http://localhost:3000',
+            stream=True,
+    ) as resp:
+        resp.raise_for_status()
+        for item in items(resp.raw, 'item'):
+            print(item)
+
+
+async def main():
+    await gather(
+        stream_data(),
+    )
+
+
+if __name__ == '__main__':
+    run(main())
+    # test: dict = {
+    #     'key0': 'val0',
+    #     'key1': {
+    #         'key2': [
+    #             {
+    #                 'key3': [
+    #                     {'2key0': '2val0'},
+    #                     {'2key1': '2val1'},
+    #                 ],
+    #             },
+    #         ],
+    #     },
+    # }
+    # for each in items(dumps(test), 'key1.key2.item.key3.item'):
+    #     print(f'main: {each}')
